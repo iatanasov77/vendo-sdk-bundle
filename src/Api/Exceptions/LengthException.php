7694942b39ca26c3df7a7e122d61a1d362d1e786
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-07-27 08:14:10 +0000

    Initial Commit.

---
+++ src/Api/Exceptions/LengthException.php
@@ -0,0 +1,5 @@
+<?php namespace Vankosoft\VendoSdkBundle\Api\Exceptions;
+
+class LengthException extends \LengthException implements ExceptionInterface
+{
+}
---
+++ src/Payum/Action/CaptureAction.php
@@ -0,0 +1,44 @@
+<?php namespace Vankosoft\VendoSdkBundle\Payum\Action;
+
+use Payum\Core\Action\ActionInterface;
+use Payum\Core\Bridge\Spl\ArrayObject;
+use Payum\Core\GatewayAwareInterface;
+use Payum\Core\GatewayAwareTrait;
+use Payum\Core\Request\Capture;
+use Payum\Core\Exception\RequestNotSupportedException;
+
+use Vankosoft\VendoSdkBundle\Payum\Request\Api\CreateTransaction;
+
+class CaptureAction implements ActionInterface, GatewayAwareInterface
+{
+    use GatewayAwareTrait;
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param Capture $request
+     */
+    public function execute( $request )
+    {
+        RequestNotSupportedException::assertSupports( $this, $request );
+
+        $model = ArrayObject::ensureArrayObject( $request->getModel() );
+
+        if ( $model['status'] ) {
+            return;
+        }
+        
+        $this->gateway->execute( new CreateTransaction( $model ) );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports( $request )
+    {
+        return
+            $request instanceof Capture &&
+            $request->getModel() instanceof \ArrayAccess
+        ;
+    }
+}
